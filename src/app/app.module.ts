fbc6fc59ac4295a1fbf1a241eb2c1ee4bf28c5e9
SGeorges <SGeorges@github.com>
2019-11-13 15:23:04 -0500

    Set-up with materialize bootstrap module and nodemodules pre-prepared.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MDBBootstrap } from './mdbootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
